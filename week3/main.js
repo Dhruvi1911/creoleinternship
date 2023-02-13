e916fc40949300d43f942be10c9c819650a80bd6
dhruvi <1911.dhruvi@gmail.com>
2023-02-13 14:55:15 +0530

    cookie,collections,error handling,module,varable file added

---
+++ week3/main.js
@@ -0,0 +1,15 @@
+// export function fun1()
+// {
+//     return "hello";
+// }
+// export const a=1;
+
+const message = () => {
+    const name = "Jesse";
+    const age = 40;
+    return name + ' is ' + age + 'years old.';
+  
+    };
+    
+    export default message;
+   
