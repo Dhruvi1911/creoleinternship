c1e8d52ae0bd38f664fb0464b42240ce9c7a05b6
dhruvi <1911.dhruvi@gmail.com>
2023-03-23 08:27:39 +0530

    usestate hook

--- week8/myreactapp3/src/Hooks/usestate.js
+++ week8/myreactapp3/src/Hooks/usestate.js
@@ -1,12 +1,51 @@
-import React,{useState} from 'react'
-export function Example(){
-    const[count,setCount]=useState(0);
-    return(
-        <>
-            <h3 style={{padding:"50px",backgroundColor:'pink' ,height:"100px",width:"100px",margin:"1.5px 700px"}}>
-                you clicked {count} times
-            </h3>
-            <button  style={{position:'absolute',left:'47.5%',marginTop:'4px',padding:"15px 32px",cursor:'pointer',borderRadius:"10px",backgroundColor:'green',color:'white',fontSize:'20px'}}onClick={()=>{setCount(count+1)}}>click me</button>
-        </>
-    );
+import React, { useEffect, useState } from "react";
+export function Example() {
+  const [count, setCount] = useState(0);
+  const [fruit, setFruit] = useState("mango");
+  const [age, setAge] = useState({age:21});
+  const [name, setName] = useState("dhruvi");
+  const handleage = () => {
+     setAge((obj)=>{ return {...obj,age:10}});
+  }
+  const useEffect=()=>{
+    document.title='you clciked',{count},'times' ;
+
+  }
+  return (
+    <>
+       
+      <h3> current age is {age.age} </h3>
+      <button
+        onClick={handleage}
+      >
+        click here to change age
+      </button>
+      <h3>hello {name}</h3>
+      <input
+        type="text"
+        placeholder="enter name here"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <h3>fruit value is {fruit}</h3>
+      <h3>tap the button to change the fruit</h3>
+      <button
+        onClick={() => {
+          setFruit("banana");
+        }}
+      >
+        click me
+      </button>
+      <h3>you clicked {count} times</h3>
+      <br />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        click me
+      </button>
+    </>
+  );
 }
